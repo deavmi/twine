d500f15aa6dd25931a90b9c405d36e27d2289ff9
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2024-01-15 15:09:47 +0200

    App
    - Run on all interfaces with link-local support

--- source/twine/app.d
+++ source/twine/app.d
@@ -6,12 +6,27 @@ void main()
 {
 	writeln("Edit source/app.d to start your project.");
 
-	import twine.six.ll : LLInterface;
+	import twine.six.ll : LLInterface, determineInterfaceAddresses, InterfaceInfo;
 	import twine.core.router;
 
 	Router r = new Router(["pubKey1", "privKey1"]);
-	LLInterface lli = new LLInterface("wlp2s0");
-	r.getLinkMan().addLink(lli);
+
+	// todo, for now i detect all interfaces which have link-local
+	InterfaceInfo[] allLinkLocalInterfaces;
+	determineInterfaceAddresses(allLinkLocalInterfaces);
+	import niknaks.debugging;
+    writeln("Will run on link-local interfaces:\n\n"~dumpArray!(allLinkLocalInterfaces));
+
+
+	// add all link-local interfaces
+	foreach(InterfaceInfo if_; allLinkLocalInterfaces)
+	{
+		LLInterface lli = new LLInterface(if_);
+		r.getLinkMan().addLink(lli);
+	}
+	
+
+	// start the router
 	r.start();
 
 	import core.thread;
