e1073d59455a3808b8d10c9bc4a8ac7b0f42853b
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2024-01-15 14:18:17 +0200

    LL
    - Working on a method for extracting all interfaces with IPv6 on them and their addresses, specficially the link-local ones

--- source/twine/six/ll.d
+++ source/twine/six/ll.d
@@ -6,6 +6,83 @@ import std.socket : Socket, Address, parseAddress, AddressFamily, SocketType, Pr
 import std.socket : SocketFlags, MSG_TRUNC, MSG_PEEK;
 import std.stdio;
 
+import core.sys.posix.sys.socket : AF_INET6;
+import twine.six.crap : getifaddrs, freeifaddrs, ifaddrs, sockaddr, sockaddr_in6, in6_addr, uint8_t;
+
+import std.string : fromStringz;
+import std.socket : Internet6Address;
+
+private bool determineInterfaceAddresses(ref string[] kak)
+{
+    ifaddrs* interfaceAddresses;
+    int stat = getifaddrs(&interfaceAddresses);
+    if(stat == 0)
+    {
+        scope(exit)
+        {
+            // todo, free allocated things here
+            freeifaddrs(interfaceAddresses);
+        }
+
+        do
+        {
+            writeln("ifa_ptr rn: ", cast(void*)interfaceAddresses);
+
+            string interfaceName = cast(string)fromStringz(interfaceAddresses.ifa_name);
+            writeln("name: "~interfaceName);
+
+            sockaddr* sAddr = interfaceAddresses.ifa_addr;
+
+            // sometimes, you may hit an `unspec` interface, then `if_addr` is 0
+            writeln("socketaddr (ptr): ", cast(void*)sAddr);
+            if(sAddr !is null)
+            {
+                // only want ipv6 (AF_6)
+                if(sAddr.sa_family == AF_INET6)
+                {
+                    writeln("found a v6 interface!: ", interfaceName);
+
+                    // cast to extended sub-object/struct
+                    sockaddr_in6* s6Addr = cast(sockaddr_in6*)sAddr;
+
+                    // only want to get da one which has la-link-local
+                    writeln("scope id: ", s6Addr.sin6_scope_id);
+                    if(s6Addr.sin6_scope_id == 18 || true) // todo, this feels broke, might need to manually check addresses
+                    {
+                        writeln("Got a lekker linj-local");
+                        // import core.sys.posix.sys.types : sockaddr_in6;
+
+                        // obtain the 16 bytes (the contained element of in6_addr
+                        // is a singular array of 16 ubytes), meaning we can cast
+                        // direct on it
+                        in6_addr addrBytesC = s6Addr.sin6_addr;
+                        ubyte[16] arr = cast(ubyte[16])addrBytesC;
+                        writeln(arr);
+
+                        
+
+
+
+                        
+                        // Internet6Address linkLocalAddr = new Internet6Address(llAddr, 0);
+                    }
+                    
+                }
+            }
+
+            // Move to next interface info struct
+            interfaceAddresses = interfaceAddresses.ifa_next;
+        }
+        while(interfaceAddresses !is null);
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 public class LLInterface : Link
 {
     private const Address mcastAddress;
@@ -21,6 +98,9 @@ public class LLInterface : Link
 
     this(string interfaceName, string mcastAddr = "ff02::1", ushort mcastPort = 1024) // should latter two be configurable?
     {
+        string[] kak;
+        determineInterfaceAddresses(kak);
+
         // Multicast socket for discovery
         this.mcastAddress = parseAddress(mcastAddr~"%"~interfaceName, mcastPort);
         this.mcastSock = new Socket(AddressFamily.INET6, SocketType.DGRAM, ProtocolType.UDP);
