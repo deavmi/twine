6ee3bca1a177c58b317e8360ee6a6b6ba3be37d7
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2024-01-15 14:18:13 +0200

    crap
    - Added some C-related imports

---
+++ source/twine/six/crap.c
@@ -0,0 +1,16 @@
+#include<sys/types.h>
+#include<ifaddrs.h>
+
+#include<arpa/inet.h>
+#include<netdb.h>
+
+#include <netinet/in.h>
+
+ 
+       #include <sys/socket.h>
+    //    #include<sys/un.h>
+
+    //    #include <stdio.h>
+    //    #include <stdlib.h>
+    //    #include <unistd.h>
+    //    #include <linux/if_link.h>
