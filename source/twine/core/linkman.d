d7b80278bd6530f8c319d2f95ee3a6537125e277
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2024-01-25 18:52:57 +0200

    LinkManager
    - Documented
    - Implemented `removeLink(Link link)`

--- source/twine/core/linkman.d
+++ source/twine/core/linkman.d
@@ -4,6 +4,12 @@ import twine.links.link;
 import std.container.slist : SList;
 import core.sync.mutex : Mutex;
 
+/** 
+ * Manages links in a manner that
+ * a single `Receiver` can be responsible
+ * for all such links attached or
+ * to be attached in the future
+ */
 public class LinkManager
 {
     private SList!(Link) links;
@@ -11,12 +17,26 @@ public class LinkManager
 
     private Receiver receiver; // make const
 
+    /** 
+     * Constructs a new `LinkManager`
+     * with the given receiver
+     *
+     * Params:
+     *   receiver = the receiver
+     * to use
+     */
     this(Receiver receiver)
     {
         this.receiver = receiver;
         this.linksLock = new Mutex();
     }
 
+    /** 
+     * Adds this link
+     *
+     * Params:
+     *   link = the link to add
+     */
     public final void addLink(Link link)
     {
         this.linksLock.lock();
@@ -29,10 +49,37 @@ public class LinkManager
         // Add link
         this.links.insertAfter(this.links[], link);
 
-        // Set its receiver
+        // Receive data from this link
         link.attachReceiver(this.receiver);
     }
 
+    /** 
+     * Removes this link
+     *
+     * Params:
+     *   link = the link to remove
+     */
+    public final void removeLink(Link link)
+    {
+        this.linksLock.lock();
+
+        scope(exit)
+        {
+            this.linksLock.unlock();
+        }
+
+        // Remove the link
+        this.links.linearRemoveElement(link);
+
+        // Don't receive data from this link anymore
+        link.removeReceiver(this.receiver);
+    }
+
+    /** 
+     * Get a list of all attached links
+     *
+     * Returns: an array
+     */
     public final Link[] getLinks()
     {
         this.linksLock.lock();
